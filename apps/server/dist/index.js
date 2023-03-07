2aac8a521d34782111370b6b7617c585bc2ca4c3
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-03-07 22:40:44 +0530

    fix: dont commit dist dir

--- apps/server/dist/index.js
+++
@@ -1,4 +0,0 @@
-import { wss } from './utils/connections.js';
-console.log(`Started WebSocket server at port ${wss.options.port}`);
-process.on('unhandledException', console.error);
-process.on('unhandledRejection', console.error);
