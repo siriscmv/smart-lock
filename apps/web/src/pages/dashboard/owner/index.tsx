bae5758183e6e7a7b8c87ea8be235a1c8960f2e0
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-08-20 15:37:01 +0000

    feat: web ui improvements

---
+++ apps/web/src/pages/dashboard/owner/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function Owner() {
+	return (
+		<div className='flex flex-col text-center'>
+			<div className='flex flex-col text-xl'>
+				<span className='text-2xl font-bold mb-6'>Owner</span>
+				<Link href='/dashboard/owner/logs' className='text-primary font-medium'>
+					View Logs
+				</Link>
+				<Link href='/dashboard/owner/password' className='text-primary font-medium'>
+					Set bypass password
+				</Link>
+				{/* <Link href='/dashboard/owner/locations' className='text-primary font-medium'>
+					Locations
+				</Link>
+				<Link href='/dashboard/owner/users' className='text-primary font-medium'>
+					Users
+				</Link> */}
+			</div>
+		</div>
+	);
+}
--- apps/web/src/pages/dashboard/owner/password.tsx
+++ apps/web/src/pages/dashboard/owner/password.tsx
@@ -1,25 +1,8 @@
 import Button from '@components/Button';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 
-interface Data {
-	distance: number;
-	d_id: number;
-	v_id: number;
-	action: 'LOCKED' | 'UNLOCKED' | 'UNLOCK_REJECTED' | 'FAIL';
-	timestamp: number;
-}
-//TODO: Also show previous logs
-
-export default function Owner() {
-	const [logs, setLogs] = useState<Data[]>([]);
-	useEffect(() => {
-		window.ws!.onmessage = (msg: MessageEvent<string>) => {
-			const log = JSON.parse(msg.data);
-			if (log.op === 'ACTION_LOG') setLogs((logs) => [log.data, ...logs]);
-		};
-	}, []);
-
+export default function Password() {
 	const [password, setPassword] = useState('');
 	const [vid, setVid] = useState<number>(0);
 
@@ -72,29 +55,6 @@ export default function Owner() {
 					}}
 					text={'Set'}
 				/>
-			</div>
-			<div className='flex flex-col text-xl'>
-				<span className='text-2xl font-bold mb-6'>Logs</span>
-				<div className='grid grid-cols-5 grid-rows-1 text-primary text-xl font-bold'>
-					<span>Driver</span>
-					<span>Vehicle</span>
-					<span>Action</span>
-					<span>Distance</span>
-					<span>Time</span>
-				</div>
-				<div className='flex flex-col'>
-					{logs.map((log) => {
-						return (
-							<div className='grid grid-cols-5 grid-rows-1' key={log.timestamp}>
-								<span>{log.d_id}</span>
-								<span>{log.v_id}</span>
-								<span>{log.action}</span>
-								<span>{log.distance}</span>
-								<span>{log.timestamp}</span>
-							</div>
-						);
-					})}
-				</div>
 			</div>
 		</div>
 	);
