5f8e345355c3897f58258f9b3977f490f522c358
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-08-20 14:35:30 +0000

    fix: PWA

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
 		<Html lang='en'>
 			<Head>
 				<link rel='manifest' href='/manifest.json' />
+				<script src='load_worker.js' />
 			</Head>
 			<body>
 				<Main />
